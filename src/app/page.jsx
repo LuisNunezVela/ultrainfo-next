5890b19f1f34a11b1ddd1d5988d57b48c4ac14b1
Luis Fernando Nuñez Vela <luisfernando.nvcb@gmail.com>
2025-07-05 06:08:49 -0400

    added components imports

---
+++ src/app/page.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Topbar from '../components/Topbar'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Stats from '../components/Stats'
+import Services from '../components/Services'
+import Work from '../components/Work'
+import Testimonials from '../components/Testimonials'
+import Faq from '../components/Faq'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Topbar />
+      <Header />
+      <Hero />
+      <About />
+      <Stats />
+      <Services />
+      <Work />
+      <Testimonials />
+      <Faq />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ffff', // your custom color
+      },
+      screens: {
+        // your custom breakpoints
+        '3xl': '1600px',
+      },
+      extend: {
+        colors: { 
+            'primary': '#009D71', // Example primary color
+            'secondary': '#F0F0F0', // Example secondary color
+            'accent': '#4a5568',
+            'white' : '#FFFFFF' // Example accent color
+        },
+        boxShadow: {
+          custom: "0px 4px 54px 10px rgba(18,19,21, 0.06)",
+        },
+      }
+    },
+  },
+  plugins: [],
+};
